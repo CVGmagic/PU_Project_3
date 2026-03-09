65cb3a1dc77c3fb6f33ce6b0cc80a751af7fa52e
cvgmagic <caius.grobbel@gmail.com>
2026-03-09 14:02:52 +0100

    Renamed snapshot_2D to plot_2D

--- 2D_renderer.py
+++ 2D_renderer.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def snapshot_2D(r: np.ndarray):
+def plot_2D(r: np.ndarray, ax):
     x = r[:, 0]
     y = r[:, 1]
-    plt.plot(x, y)
-    plt.show()
+    ax.plot(x, y)
