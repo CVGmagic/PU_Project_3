eb89d102d67580100e1dace679fd5c7275c26a76
cvgmagic <caius.grobbel@gmail.com>
2026-03-09 17:07:25 +0100

    Added empty __init__.py file to renderers, to make it appear as package

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 r = random_shape_creator_2D.create_circle_2D(np.array([0, 0]), 1, 1000)
 print(r)
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
 renderer_2D.plot_points_2D(r, ax)
 plt.show()
