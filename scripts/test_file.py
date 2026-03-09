6f88b2e4c9c84790a1cd72b88e38d193ecbbe848
cvgmagic <caius.grobbel@gmail.com>
2026-03-09 17:19:18 +0100

    Tested new 3D functions, all working as intended

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -1,12 +1,12 @@
-from simulation import random_shape_creator_2D
-from renderers import renderer_2D
+from simulation import random_shape_creator_2D, random_shape_creator_3D
+from renderers import renderer_2D, renderer_3D
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-r = random_shape_creator_2D.create_circle_2D(np.array([0, 0]), 1, 1000)
+r = random_shape_creator_3D.create_sphere_3D(np.array([0, 0, 0]), 1, 1000)
 print(r)
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
-renderer_2D.plot_points_2D(r, ax)
+renderer_3D.plot_points_3D(r, ax)
 plt.show()
