25ce560ae270c873ce3ddd5c96c88fff29662fcf
Maïc <maic.morel@bluewin.ch>
2026-03-15 20:24:53 +0100

    finishing acceleration_calculator + checking with test_file

---
+++ src/acceleration/acceleration_calculator_3D.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+
+# calculates how strong two points attract
+def calculate_acc(points: np.ndarray, a: int, b: int):
+    r = math.sqrt(points[a]**2 + points[b]**2)
+    single_acc = 1 / r**3 * (points[b]-points[a])
+    # F = G * m1 * m2 / r**3 * (points[b]-points[a]), a = F / m1 = G * m2 / r**3 * (points[b]-points[a])
+    # define m2 = 1/G = 1/(6.6743 × 10**(-11)) = 15.0 * 10**9 kg
+    return single_acc
+# returns the acceleration vector (incomplete acceleration)
+
+# calculates the total acceleration on a point
+def calculate_complete_acc(points: np.ndarray, n: int):
+    total_acc = np.zeros(3)
+    for i in points:
+        if i != n:
+            total_acc += calculate_acc(points, n, i)
+    return total_acc
+# returns the total acceleration on a single point as a vector
+
+# creates an array with the total acceleration of every point
+def create_array_acc(points: np.ndarray):
+    acc = np.zeros((len(points), 3))
+    for i in acc:
+        acc[i] = calculate_complete_acc(points, i)
+    return acc
