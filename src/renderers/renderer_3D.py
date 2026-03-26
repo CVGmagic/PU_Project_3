85aaa76c0fb5c07f66775c1b6ec4fc57538270cd
cvgmagic <caius.grobbel@gmail.com>
2026-03-26 21:23:48 +0100

    Added plot_points_3D_PyVis function and renamed plot_points_3D to plot_points_3D_plt

--- src/renderers/renderer_3D.py
+++ src/renderers/renderer_3D.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from vispy import scene, app
 
-def plot_points_3D(r: np.ndarray, ax):
+def plot_points_3D_plt(r: np.ndarray, ax) -> None:
     x = r[:, 0]
     y = r[:, 1]
     z = r[:, 2]
 
     ax.scatter(x, y, z, linestyle="", marker="o")
 
+    return
+
+
+def plot_points_3D_PyVis(r: np.ndarray, scatter, sizes=np.array([])) -> None:
+    """
+    Creates a 3D plot on scatter using the positions in r, and the sizes in sizes.
+    If no sizes are provided, 1 is used as a default size
+    """
+    if sizes.size() == 0:
+        sizes = np.ones((r.size()))
+    elif sizes.size() != r.size():
+        raise ValueError("r and sizes must have the same length")
+
+    scatter.set_data(positions, size=sizes, face_color=(1, 1, 1, 1))
+
+    return
