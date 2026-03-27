9660ef9d415d2d0228528da131f465a7eda452d1
Maïc <maic.morel@bluewin.ch>
2026-03-27 15:55:00 +0100

    h

--- scripts/test_file.py
+++ scripts/test_file.py
@@ -51,11 +51,11 @@ view.add(scatter) # adds scatter (basically points) to view
 
 """ add n -> n+1 and add line 57 when we add the sum but also add the coordinate of the sun that it works
 len(m) == len(r)"""
-dt = 0.01
+dt = 0.001
 m = np.full(n, 100) # creates array with n elements and (masses of 100)
 v = np.zeros((n, 3)) # v has n elements in 3D filled with 0's
 a = calc_acc_rep_np(r, m) # calculates the acceleration of every single r based on their location (r)
-#m[n] = 333000000
+#m[n] = 33300000
 v += a * dt / 2 # updates v
 
 def update(event):
