64647803f5372efdbac5715273e8b6917f18205a
cvgmagic <caius.grobbel@gmail.com>
2026-03-27 22:20:47 +0100

    Fixed helper functions and acceleration functions. Way smaller energy fluctuations now.

--- src/simulation/energy_calculator.py
+++ src/simulation/energy_calculator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from simulation.constants import G, epsilon
 
+
 def get_gravitational_energy(r, m) -> float:
     diff = r[:, None, :] - r[None, :, :]
     dist = np.linalg.norm(diff, axis=-1) # Gets the lengths of all differences
@@ -10,9 +11,10 @@ def get_gravitational_energy(r, m) -> float:
     total_energy = np.sum(np.triu(pairwise_energy, k=1)) # Takes the sum of all elements above the diagonal (every pair is only included once and both particles have to be different)
     return total_energy
 
-def get_potential_energy(m, v) -> float:
-    return np.sum(v * v * m / 2)
+
+def get_kinetik_energy(m, v) -> float:
+    return np.sum(np.sum(v * v, axis=-1) * m / 2)
 
 
 def get_total_energy(r, m, v) -> float:
-    return get_potential_energy(m, v) + get_gravitational_energy(r, m)
+    return get_kinetik_energy(m, v) + get_gravitational_energy(r, m)
