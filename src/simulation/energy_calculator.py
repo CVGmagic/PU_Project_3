76ab89863f4460de2352ca324766e54c9bbc5f82
cvgmagic <caius.grobbel@gmail.com>
2026-03-27 22:04:49 +0100

    Made some energy helper functions

--- src/simulation/energy_calculator.py
+++ src/simulation/energy_calculator.py
@@ -1,7 +1,18 @@
 import numpy as np
 from simulation.constants import G, epsilon
 
-def get_gravitational_energy(r, m):
+def get_gravitational_energy(r, m) -> float:
+    diff = r[:, None, :] - r[None, :, :]
+    dist = np.linalg.norm(diff, axis=-1) # Gets the lengths of all differences
+
+    pairwise_mass = m[:, None] * m[None, :]
+    pairwise_energy = -G * pairwise_mass
+    total_energy = np.sum(np.triu(pairwise_energy, k=1)) # Takes the sum of all elements above the diagonal (every pair is only included once and both particles have to be different)
+    return total_energy
+
+def get_potential_energy(m, v) -> float:
+    return np.sum(v * v * m / 2)
 
 
-def get_total_energy(r, m, v):
+def get_total_energy(r, m, v) -> float:
+    return get_potential_energy(m, v) + get_gravitational_energy(r, m)
