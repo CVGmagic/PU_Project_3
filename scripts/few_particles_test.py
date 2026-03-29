26a6cd6bb6a3833217d67e49a8bf8b92ea55bc71
cvgmagic <caius.grobbel@gmail.com>
2026-03-29 11:34:15 +0200

    Fixed acceleration_calculator_3D.py

--- scripts/few_particles_test.py
+++ scripts/few_particles_test.py
@@ -1,6 +1,5 @@
-from PycharmProjects.Flavio_Beispielprojekt.main import compute_accelerations
 from vispy import scene, app
-from acceleration.acceleration_calculator_3D import calculate_attractive_acceleration
+from acceleration.acceleration_calculator_3D import calculate_gravitational_acceleration
 import numpy as np
 from renderers import renderer_3D
 from simulation.energy_calculator import get_total_energy
@@ -26,7 +25,7 @@ dt = 0.01
 v = np.zeros((r.shape[0], 3))
 v[1] = [0, 14, 0]
 v[2] = [0, 14, 0]
-a = compute_accelerations(r, m)
+a = barnes_hut.compute_accelerations(r, m)
 v += a * dt / 2
 
 def update(event):
@@ -38,7 +37,7 @@ def update(event):
     dist = r[0] - r[1]
     print(get_total_energy(r, m, v))
 
-    a = calculate_gravitational_acceleration(r, m)
+    a = barnes_hut.compute_accelerations(r, m)
 
     v += a * dt
 
--- src/acceleration/acceleration_calculator_3D.py
+++ src/acceleration/acceleration_calculator_3D.py
@@ -47,16 +47,16 @@ def calc_acc_rep_np(r, m):
     return a
 
 
-def calculate_complete_acceleration(r, m, en_rel):
+def calculate_gravitational_acceleration(r, m, en_rel):
     """
     Calculates an attractive acceleration between points.
     Has epsilon, but no short distance repulsion.
     """
     diff = r[:, None, :] - r[None, :, :]  # stores 3D-vector between every two-point combination
-    dist_sq = np.sum(diff * diff, axis=-1) + eps_sq  # stores 1D distance between evry two-point combination squared
+    dist_sq = np.sum(diff * diff, axis=-1)  # stores 1D distance between evry two-point combination squared
     np.fill_diagonal(dist_sq, np.inf)  # changes distance of two-point combination of same points to inf
 
-    inv_dist_3 = 1 / (dist_sq * np.sqrt(dist_sq))
+    inv_dist_3 = 1 / ((dist_sq + eps_sq) * np.sqrt(dist_sq + eps_sq))
     inv_dist_8 = -en_rel / dist_sq**4
 
     a = -np.sum(diff * (inv_dist_3[:, :, None] - inv_dist_8[:, :, None]) * m[None, :, None], axis = 1)
