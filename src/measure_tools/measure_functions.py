Fixed energy relation issue, where energy relation had wrong sign. Not a single explosion occuring now

src/measure_tools/measure_functions.py
@@ -73,9 +73,9 @@ def benchmark_computation(particle_counts: list[int], do_standard=True, do_barne
     standard_times = np.empty(n)
     barnes_hut_times = np.empty((n, len(opening_angles)))
     r_init = create_relaxed_sphere_3D()
-    v_init = np.random.random((n, 3)) * 15
-    m_init = np.random.random(n) * 15
-
+    v_init = np.random.random((n, 3))
+    m_init = np.random.random(n) * 100
+    e_rel = 8.6e-14 # Just an example value from one real simulation
     for i, particle_count in enumerate(particle_counts):
         if do_standard:
             r = np.copy(r_init)

src/simulation/energy_calculator.py
@@ -48,9 +48,9 @@ def calculate_potential_energies(r, m):
     # Using the 0.5 here to account for double-counting pairs
     gravity_potential_energy = -0.5 * np.sum(mass_matrix * (1 / dist)) * G
 
-    # 4. Pressure Energy (Integral of 1/r^8 is 1/(7 * r^7))
-    # r^7 = (dist_sq^3 * dist)
-    pressure_potential_energy = 0.5 * np.sum(mass_matrix * (1 / (7 * dist_sq ** 3 * dist)))
+    # 4. Pressure Energy (Integral of 1/r^8 is -1/(7 * r^7))
+    # r^5 = (dist_sq^3 * dist)
+    pressure_potential_energy = -0.5 * np.sum(mass_matrix * (1 / (4 * dist_sq ** 3 * dist)))
 
     return gravity_potential_energy, pressure_potential_energy
 